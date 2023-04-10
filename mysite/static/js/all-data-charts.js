ebeb16827428900609b0b7e9c901bb17877703d8
Samiksha-Pansare <pansaresamiksha@gmail.com>
2023-04-10 21:52:39 +0530

    Added 1 Page

--- mysite/static/js/all-data-charts.js
+++ mysite/static/js/all-data-charts.js
@@ -1,25 +1,28 @@
-Chart.defaults.global.legend.labels.usePointStyle = true;
 
-  
-  window.onload = function() {
-    
-    Chart.defaults.global.legend.labels.usePointStyle = true;
-    var ctx = document.getElementById("visit-sale-chart").getContext("2d");
 
-      var gradientStrokeViolet = ctx.createLinearGradient(0, 0, 0, 181);
-      gradientStrokeViolet.addColorStop(0, 'rgba(218, 140, 255, 1)');
-      gradientStrokeViolet.addColorStop(1, 'rgba(154, 85, 255, 1)');
-      var gradientLegendViolet = 'linear-gradient(to right, rgba(218, 140, 255, 1), rgba(154, 85, 255, 1))';
-      
-      var gradientStrokeBlue = ctx.createLinearGradient(0, 0, 0, 360);
-      gradientStrokeBlue.addColorStop(0, 'rgba(54, 215, 232, 1)');
-      gradientStrokeBlue.addColorStop(1, 'rgba(177, 148, 250, 1)');
-      var gradientLegendBlue = 'linear-gradient(to right, rgba(54, 215, 232, 1), rgba(177, 148, 250, 1))';
 
-      var gradientStrokeRed = ctx.createLinearGradient(0, 0, 0, 300);
-      gradientStrokeRed.addColorStop(0, 'rgba(255, 191, 150, 1)');
-      gradientStrokeRed.addColorStop(1, 'rgba(254, 112, 150, 1)');
-      var gradientLegendRed = 'linear-gradient(to right, rgba(255, 191, 150, 1), rgba(254, 112, 150, 1))';
+
+// new Chart(document.querySelector('#doughnutChart'), {
+//   type: 'doughnut',
+//   data: {
+//     labels: [
+//       'White',
+//       'Black',
+//       'Grey'
+//     ],
+//     datasets: [{
+//       label: 'My First Dataset',
+//       data: [300, 50, 100],
+//       backgroundColor: [
+//         'rgb(255, 99, 132)',
+//         'rgb(54, 162, 235)',
+//         'rgb(255, 205, 86)'
+//       ],
+//       hoverOffset: 4
+//     }]
+//   }
+// });
+  window.onload = function() {   
     var barChartallData = {
     
         labels: [
@@ -130,4 +133,5 @@ Chart.defaults.global.legend.labels.usePointStyle = true;
     });
     
   };
-  
+
+
